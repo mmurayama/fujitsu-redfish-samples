54b378dd6492fe1ccfeb2ab924e81ec5cb5e0df9
Masahiro Murayama <mmurayama@us.fujitsu.com>
2020-07-24 17:00:00 -0700

    removed session login/logout, and added the '-r' option.

--- scripts/get_sel.py
+++ scripts/get_sel.py
@@ -9,8 +9,12 @@
   - Show all events
   $ python get_sel.py -i 192.168.10.10 -u admin -p admin
 
+  - Show all events (ascending)
+  $ python get_sel.py -i 192.168.10.10 -u admin -p admin -r
+
   - Show the specified event only
   $ python get_sel.py -i 192.168.10.10 -u admin -p admin -e 118
+  
 """
 import sys
 import argparse
@@ -20,38 +24,28 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 __author__ = "Masahiro Murayama"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 
-def get_sel(irmc, user, password, event_id):
-    session = requests.Session()
-    session.verify = False
+def get_sel(irmc, user, password, ascend, event_id):
+    headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
 
-    header = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-    payload = {'UserName': user, 'Password': password}
-    sessions_url = "https://{}/redfish/v1/SessionService/Sessions".format(irmc)
-
-    response = session.post(sessions_url,
-                            headers=header,
-                            data=json.dumps(payload))
-
-    if response.status_code != 201:
-        print("ERROR: Failed to create a session.")
-        print("DETAILS: {}".format(response.json()['error']['message']))
-        sys.exit()
-
-    session.headers.update({"X-Auth-Token": response.headers["X-Auth-Token"]})
-    session_info = response.headers["Location"]
-
-    response = session.get("https://{}/redfish/v1/Managers/iRMC/LogServices/SystemEventLog/Entries".format(irmc))
+    response = requests.get(
+        "https://{}/redfish/v1/Managers/iRMC/LogServices/SystemEventLog/Entries".format(irmc),
+        headers=headers,
+        auth=(user, password),
+        verify=False
+    )
 
     if response.status_code != 200:
         print("ERROR: Failed to get the system event log information from the iRMC at {}".format(irmc))
         print("DETAILS: {}".format(response.json()['error']['message']))
-        session.delete("https://{}{}".format(irmc, session_info))
         sys.exit()
 
     sel_entries_list = response.json()['Members']
+
+    if ascend == True:
+        sel_entries_list.reverse()
 
     if event_id is None:
         print("ID".rjust(3) + " | " + "Data/Time".ljust(25) +
@@ -75,8 +69,6 @@ def get_sel(irmc, user, password, event_id):
         if found == False:
             print("ERROR: Could not find the specified event (ID: {})".format(event_id))
 
-    session.delete("https://{}{}".format(irmc, session_info))
-
 
 def main():
     parser = argparse.ArgumentParser()
@@ -92,15 +84,17 @@ def main():
     parser.add_argument('-e', '--event',
                         type=int,
                         help="Show the event specified by the ID")
+    parser.add_argument('-r',
+                        action='store_true',
+                        help="Ascending the events")
     args = parser.parse_args()
     irmc = args.irmc
     user = args.user
     password = args.password
     event_id = args.event
+    ascend = args.r
 
-    get_sel(irmc, user, password, event_id)
-
-    return 0
+    get_sel(irmc, user, password, ascend, event_id)
 
 
 if __name__ == '__main__':
